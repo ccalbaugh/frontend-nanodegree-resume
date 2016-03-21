d67f7f0667b13f8f583562d0cdf3f3184471bbe9
Caden Albaugh <CadenAlbaugh@gmail.com>
2016-03-21 11:51:47 -0500

    Commit the basic completed resume

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -85,16 +71,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  locClicks(x, y);
 });
 
-
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
 var map;    // declares a global map variable
 
 
@@ -176,7 +157,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -238,11 +219,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,33 +1,19 @@
-// var name = "Caden Albaugh";
-// var formattedName = HTMLheaderName.replace("%data%", name);
-
-// var role = "Web Developer";
-// var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
-// $("#main").append(bio.name);
-// $("#main").append(work["position"]);
-// $("#main").append(education.schools[0].name);
-// $("#main").append(bio.skills);
-// $("#main").append(bio.skills[0]);
-
 var bio = {
 	"name": "Caden Albaugh",
 	"role": "Web Developer",
-	contact: {
+	"contacts": {
 		"mobile": "319-330-1742",
 		"email": "CadenAlbaugh@gmail.com",
 		"GitHub": "https://github.com/ccalbaugh",
 		"twitter": "@CadenAlbaugh",
-		"location": "Iowa City"
+		"location": "Iowa City, IA"
 	},
 	"welcomeMessage": "Hey Everyone!",
 	"skills": [
-		"skillz"
+		"Backflips", "Regeneration", "Telepathy", "HopScotch"
 	],
-	"bioPic": "images/me.jpg"
-};
+	"bioPic": "images/197x148.gif"
+}
 
 var work = {
 	"jobs": [
@@ -35,15 +21,22 @@ var work = {
 			"employer": "Albaugh PHC",
 			"title": "HVAC Technician",			
 			"location": "Tipton, IA",
-			"datesWorked": "2013-2016",
-			"description": "Just as it sounds"
+			"datesWorked": "October, 2013 - Present",
+			"description": "I provide cold to those whose flame cannot be extinguished, and provide heat to those with ice in their hearts!"
 		},
 		{
 			"employer": "Albaugh PHC",
 			"title": "HVAC Technician",			
 			"location": "Tipton, IA",
-			"datesWorked": "2013-2016",
-			"description": "Just as it sounds"
+			"datesWorked": "October, 2013 - Present",
+			"description": "I provide cold to those whose flame cannot be extinguished, and provide heat to those with ice in their hearts!"
+		}, 
+		{			
+			"employer": "Albaugh PHC",
+			"title": "HVAC Technician",			
+			"location": "Denver, CO",
+			"datesWorked": "October, 2013 - Present",
+			"description": "I provide cold to those whose flame cannot be extinguished, and provide heat to those with ice in their hearts!"
 		}
 	]
 }
@@ -52,18 +45,18 @@ var education = {
 	"schools": [
 		{
 			"name": "University of Iowa",
-			"location": "Iowa City",
+			"location": "Iowa City, IA",
 			"degree": "BA",
 			"major" : "International Studies",
 			"minor" : "Portuguese",
-			"graduated": "2012"
+			"dates": "2010 - 2012"
 		},
 		{
 			"name": "Udacity",
 			"location": "Online",
 			"degree": "Nano Degree",
 			"major" : "Front End Developer",
-			"graduated": "2016"
+			"dates": "2016 - Present"
 		}	
 	]
 }
@@ -89,6 +82,109 @@ var projects = {
 	]
 }
 
-if (bio.skills.length > 0) {
-	document.getElementById('header').append()
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header:first").prepend(formattedName + formattedRole);
+var formattedLocal = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#topContacts").append(formattedLocal);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+$("#topContacts").append(formattedTwitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.GitHub);
+$("#topContacts").append(formattedGithub);
+
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$("#topContacts").append(formattedBioPic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+$("#topContacs").append(formattedWelcomeMsg);
+
+
+
+
+
+if(bio.skills.length > 0) {
+      $("#header").append(HTMLskillsStart);
+      var formattedSkills = HTMLskills.replace("%data%", bio.skills[0]);
+      $("#skills").append(formattedSkills);
+      formattedSkills = HTMLskills.replace("%data%", bio.skills[1]);
+      $("#skills").append(formattedSkills);
+      formattedSkills = HTMLskills.replace("%data%", bio.skills[2]);
+      $("#skills").append(formattedSkills);
+      formattedSkills = HTMLskills.replace("%data%", bio.skills[3]);
+      $("#skills").append(formattedSkills);
+    }
+
+function displayWork() {
+	for (key in work.jobs) {
+		if (work.jobs.hasOwnProperty(key)) {
+			$("#workExperience").append(HTMLworkStart);
+			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[key].employer);
+			var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[key].title);
+			var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[key].datesWorked);
+			var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[key].location);
+			var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[key].description);
+			var formattedWorkInfo = formattedEmployer + formattedTitle + formattedDatesWorked + formattedLocation + formattedDescription;
+
+			$(".work-entry:last").append(formattedWorkInfo);
+		}
+	}
 }
+
+displayWork();
+
+$(internationalizeButton).click(function inName(name) {
+	name = name.trim().split(" ");
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+	return name[0] + " " + name[1];
+});
+
+$("#main").append(internationalizeButton);
+
+function displaySchool() {
+	for (key in education.schools) {
+		if (education.schools.hasOwnProperty(key)) {
+			$("#education").append(HTMLschoolStart);
+			var formattedName = HTMLschoolName.replace("%data%", education.schools[key].name);
+			var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[key].degree);
+			$(".education-entry:last").append(formattedName + formattedDegree);
+			var formattedDates = HTMLschoolDates.replace("%data%", education.schools[key].dates);
+			$(".education-entry:last").append(formattedDates);
+			var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[key].major);
+			$(".education-entry:last").append(formattedMajor);
+			var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[key].location);
+			$(".education-entry:last").append(formattedLocation);
+		}
+	}	
+}
+
+displaySchool();
+
+function displayProjects() {
+	for (key in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+		var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[key].title);
+		$(".project-entry:last").append(formattedProjectTitle);
+		var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[key].dates);
+		$(".project-entry:last").append(formattedProjectDates);
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[key].description);
+		$(".project-entry:last").append(formattedProjectDescription);
+
+		if (projects.projects[key].images.length > 0) {
+			for (image in projects.projects[key].images) {
+				var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[key].images[image]);
+				$(".project-entry:last").append(formattedProjectImage);
+			}
+		}
+	}
+};
+
+displayProjects();
+
+
+$("#mapDiv").append(googleMap);
